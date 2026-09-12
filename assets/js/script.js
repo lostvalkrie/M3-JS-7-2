65a395e3252ff8543dbfcfe11f14ffd7dfb03abc
lostvalkrie <lostvalkrie08@gmail.com>
2026-09-12 22:59:16 +0300

    Complete task 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const printNumbers = (from, to, interval) => {
+  let now = from;
+
+  const timerId = setInterval(() => {
+    console.log(now);
+
+    if (now === to) {
+      clearInterval(timerId);
+    }
+    now++;
+  }, interval);
+};
+
+printNumbers(1, 5, 1000);
+
+const printNumbersByTimeout = (from, to, interval) => {
+  let now = from;
+
+  setTimeout(function func() {
+    console.log(now);
+    if (now < to) {
+      setTimeout(func, interval);
+    }
+    now++;
+  }, interval);
+};
+
+printNumbersByTimeout(6, 10, 1000);
