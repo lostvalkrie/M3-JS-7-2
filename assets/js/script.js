59ed58541c0dd1f610086cba86fc9ef6aac1c95e
lostvalkrie <lostvalkrie08@gmail.com>
2026-09-12 23:43:09 +0300

    Complete task 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,31 @@ const printNumbersByTimeout = (from, to, interval) => {
 };
 
 printNumbersByTimeout(6, 10, 1000);
+
+function setImage({ message }) {
+  if (!article.contains(image)) {
+    article.prepend(image);
+  }
+  image.src = `${message}`;
+}
+
+const article = document.createElement("article");
+article.classList.add("dogs-article");
+
+const image = document.createElement("img");
+image.classList.add("dog-img");
+image.alt = "random dog";
+
+const generateDogBtn = document.createElement("button");
+generateDogBtn.textContent = "Generate dog image";
+generateDogBtn.classList.add("dogs-btn");
+generateDogBtn.addEventListener("click", () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => setImage(data));
+});
+
+article.append(generateDogBtn);
+document.body.append(article);
+
+generateDogBtn.click();
